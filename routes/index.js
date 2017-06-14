10adea4558a95ff4a4cc40dbe913368fa21027c1
stasmts@gmail.com <qazwsx123>
2017-06-14 16:57:50 +0300

    .gitignore update

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('./controllers/storeController');
+
+
+router.get('/', storeController);
+
+
 
 // Do work here 
-router.get('/', (req, res) => {
-	//res.json(req.query);
-	res.render('hello', {
-		name: 'wes',
-		dog: req.query.dog,
-		title: 'I love food'
-	});
-});
+// router.get('/', (req, res) => {
+// 	//res.json(req.query);
+// 	res.render('hello', {
+// 		name: 'wes',
+// 		dog: req.query.dog,
+// 		title: 'I love food'
+// 	});
+// });
 
 // router.get('/reverse/:name', (req, res) => {
 //   const reverse = [...req.params.name].reverse().join('');
